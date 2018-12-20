73f5859f92ba27faa2221fbb8450b8e8dcaf9d3c
Miguelangel <silvermiguel96@gmail.com>
2018-12-19 22:59:27 -0500

    Comienzo Proyecto

---
+++ pruebas.js
@@ -0,0 +1,28 @@
+const altercado = ["abc4" , "123", "xyz"]
+
+
+function asignarEspacioParaPalabras (frases) {
+    const maximasPalabras = frases.length
+    let  numeroDePalabras = 0
+    // Numero  mayor 
+    frases.map( function(frase, index ){
+        if (frase.length > numeroDePalabras){
+            numeroDePalabras = frase.length
+        }
+    })
+    // Recorrido del frases
+    let numeroRecorrido = 0
+    let numeroPosicionPalabra = 0
+    let i = 0
+    let almacenarPalabra = ''
+    while(i < numeroRecorrido){
+
+
+    }
+
+
+}
+asignarEspacioParaPalabras(altercado)
+
+// Crear un alamcenamiento mirar cua es mas grande  mientras controlador  do while 
+
