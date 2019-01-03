7810ea624085e96df28c193bec56be9b2c1134da
Miguelangel <silvermiguel96@gmail.com>
2019-01-03 18:09:54 -0500

    Usando Pug como template engine

--- prueba2.js
+++
@@ -1,5 +0,0 @@
-function findLongestWordLength(str) {
-  return str.split(' ').reduce(function( x, y) { return Math.max( x, y.length)}, 0);
-}
-
-console.log(findLongestWordLength("The quick brown fox jumped over the lazy dog"));
