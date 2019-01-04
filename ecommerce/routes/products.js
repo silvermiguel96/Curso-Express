f2eda624dcf90517c924b1c939921201eac50e77
Miguelangel <silvermiguel96@gmail.com>
2019-01-03 23:26:01 -0500

    Creando nuestro layoutde productos usando Pug

--- ecommerce/routes/products.js
+++ ecommerce/routes/products.js
@@ -5,11 +5,13 @@ const router = express.Router();
 const products = [
   {
     name: "Red Shoes",
-    price: 75
+    price: 75,
+    image: "https://di2ponv0v5otw.cloudfront.net/posts/2018/07/15/5b4b99a9fe51518f9cf01863/m_5b4b99b5a31c333d40eeb608.jpg"
   },
   {
     name: "Black bike",
-    price: 300
+    price: 300,
+    image: "https://black-bikes.com/wp-content/uploads/2017/03/gents-bike.jpg"
   }
 ]
 
